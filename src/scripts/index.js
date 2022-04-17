943d72d772b1f9e0ddb94a369e425fbb5b8471fb
Luiz Gustavo <lgdeveloper@outlook.com>
2022-04-17 00:24:55 -0300

    feat: added function to check active item and go to order page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,38 @@
 import { productsModule } from './products'
 import { orderModule } from './order'
-
+import { carouselModule } from './carousel'
 
 (function() {
 
-  productsModule.loadFolders();  
+  carouselModule.init();
+  productsModule.init();  
+  orderModule.init();
 
-  [...document.querySelectorAll('.item')].map(item => item.addEventListener('click', function (e) {
+  [...document.querySelectorAll('.item')].map(item => item.addEventListener('click', (e) => {
     const target = e.currentTarget;
+    
+    const itemsBroth = [...document.querySelectorAll("[data-type='broth']")];
+    const itemsMeat = [...document.querySelectorAll("[data-type='meat']")];
+
+    function checkActiveElements (arrayOfElements) {
+      arrayOfElements.map(el => {
+        if(el !== target && el.dataset.type === target.dataset.type) {
+          el.classList.remove('active')
+        } else {
+          target.classList.add('active');
+        }
+      })
+    }    
+
+    checkActiveElements(itemsBroth);
+    checkActiveElements(itemsMeat);
 
     orderModule.setOrder(target.dataset.type, target.dataset.id);
+    
   }))
+
+  document.querySelector('#order').addEventListener('click', () => {
+    orderModule.success();
+  })
 
 })()
