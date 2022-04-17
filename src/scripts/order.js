847b920976aa7f66ab3f9470460eff4df7c7cec9
Luiz Gustavo <lgdeveloper@outlook.com>
2022-04-17 00:24:07 -0300

    feat: added function to reset order and check if order completed

--- src/scripts/order.js
+++ src/scripts/order.js
@@ -1,14 +1,38 @@
 export const orderModule = (function () {
   const order = {};
 
-  const generateOrder = (type, id) => {
+  const resetOrder = () => {
+    sessionStorage.removeItem('order');
+  }
+
+  const saveOrder = (type, id) => {
     order[type] = id;
     sessionStorage.setItem('order', JSON.stringify(order));
   }
 
+  const placeOrder = () => {
+    const order = sessionStorage.getItem('order');
+
+    if(order) {
+
+      const list = JSON.parse(order);
+      
+      if(Object.keys(list).length === 2) {
+        window.location.href = '/success'
+      }
+  
+    }
+  }
+
   return {
+    init: function () {
+      resetOrder()
+    },
     setOrder: function (type, id) {
-      generateOrder(type, id)
+      saveOrder(type, id)
+    },
+    success: function() {
+      placeOrder()
     }
   }
 })()
